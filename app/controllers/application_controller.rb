89376149fb8296a5913ddef6193b312560242567
claudiu <claudiu.garba@gmail.com>
2014-02-15 15:49:32 +0100

    restrict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :secure_access
+
+  private
+
+  def secure_access
+    api_key = ApiKey.find_by_auth_token(params[:auth_token])
+    head :unauthorized unless api_key
+  end
 end
